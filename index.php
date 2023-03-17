4c3db1cccab313a1e2d1ebcd73dbddc8493c3dfc
agungwahyu <69448014+agungwahyu14@users.noreply.github.com>
2023-03-17 23:17:50 +0800

    create project,skill

--- index.php
+++ index.php
@@ -1,12 +1,177 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Agung Wahyu| Portofolio</title>
 </head>
+
 <body>
-    
+    <!-- Awal Navbar -->
+    <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand text-light" href="#">Agung Wahyu| Portofolio</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link text-light" aria-current="page" href="#">HOME</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-light" href="#">ABOUT ME</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-light" href="#">PROJECT</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-light ">SKILLS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-light ">CONTACT</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- Akhir Navbar -->
+
+    <!-- Awal Home -->
+    <section class="jumbotron text-center">
+
+
+        <img src="img/download.png" alt="profile" width="250" class="rounded-circle img-thumbnail" />
+        <h1 class="mt-3">Agung Wahyu</h1>
+        <h5 class="mt-2">Developer | ITB Stikom Bali</h5>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path fill="#ffff" fill-opacity="1" d="M0,128L21.8,117.3C43.6,107,87,85,131,106.7C174.5,128,218,192,262,218.7C305.5,245,349,235,393,218.7C436.4,203,480,181,524,165.3C567.3,149,611,139,655,154.7C698.2,171,742,213,785,213.3C829.1,213,873,171,916,144C960,117,1004,107,1047,122.7C1090.9,139,1135,181,1178,208C1221.8,235,1265,245,1309,229.3C1352.7,213,1396,171,1418,149.3L1440,128L1440,320L1418.2,320C1396.4,320,1353,320,1309,320C1265.5,320,1222,320,1178,320C1134.5,320,1091,320,1047,320C1003.6,320,960,320,916,320C872.7,320,829,320,785,320C741.8,320,698,320,655,320C610.9,320,567,320,524,320C480,320,436,320,393,320C349.1,320,305,320,262,320C218.2,320,175,320,131,320C87.3,320,44,320,22,320L0,320Z"></path>
+        </svg>
+    </section>
+
+    <!-- Akhir Home -->
+
+
+    <!-- Awal About -->
+    <section id="about">
+        <div class="container text-center mb-4">
+            <div class="row">
+                <h2>ABOUT ME</h2>
+            </div>
+        </div>
+        <div class="container text-center">
+            <div class="row">
+                <div class="col">
+                    <img src="img/download.png" alt="profile" width="250" class="rounded-circle img-thumbnail" />
+                </div>
+                <div class="col">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas enim ipsam quisquam in magnam nisi quia voluptatibus saepe possimus eaque ad non corporis distinctio tenetur, suscipit quod quos iste quis?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deserunt aspernatur et delectus alias vitae perspiciatis exercitationem provident impedit minus necessitatibus odit aliquam dolore, rerum, tenetur, labore quod. Corrupti, ipsa.
+                </div>
+                <div class="col">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore hic quas reprehenderit doloremque temporibus at iste deserunt minima vero pariatur officia nostrum blanditiis debitis, maxime fugiat, doloribus perferendis facilis quasi!locale_filter_matches Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis esse autem vero beatae fugiat libero, explicabo ipsa dignissimos eos velit laboriosam non sit praesentium? Est provident ad tempore aliquam reprehenderit?
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+    <!-- Akhir About -->
+
+    <!-- Awal Project -->
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path fill="#f3f4f5" fill-opacity="1" d="M0,128L21.8,117.3C43.6,107,87,85,131,106.7C174.5,128,218,192,262,218.7C305.5,245,349,235,393,218.7C436.4,203,480,181,524,165.3C567.3,149,611,139,655,154.7C698.2,171,742,213,785,213.3C829.1,213,873,171,916,144C960,117,1004,107,1047,122.7C1090.9,139,1135,181,1178,208C1221.8,235,1265,245,1309,229.3C1352.7,213,1396,171,1418,149.3L1440,128L1440,320L1418.2,320C1396.4,320,1353,320,1309,320C1265.5,320,1222,320,1178,320C1134.5,320,1091,320,1047,320C1003.6,320,960,320,916,320C872.7,320,829,320,785,320C741.8,320,698,320,655,320C610.9,320,567,320,524,320C480,320,436,320,393,320C349.1,320,305,320,262,320C218.2,320,175,320,131,320C87.3,320,44,320,22,320L0,320Z"></path>
+    </svg>
+    <section id="project">
+        <div class="container text-center mb-4">
+            <div class="row">
+                <h2>MY PROJECT</h2>
+            </div>
+        </div>
+        <div class="container my-5">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card mb-3">
+                        <img src="https://via.placeholder.com/350x200" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Proyek 1</h5>
+                            <p class="card-text">Deskripsi singkat proyek 1.</p>
+                            <a href="#" class="btn btn-primary">Lihat Detail</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card mb-3">
+                        <img src="https://via.placeholder.com/350x200" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Proyek 2</h5>
+                            <p class="card-text">Deskripsi singkat proyek 2.</p>
+                            <a href="#" class="btn btn-primary">Lihat Detail</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card mb-3">
+                        <img src="https://via.placeholder.com/350x200" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Proyek 3</h5>
+                            <p class="card-text">Deskripsi singkat proyek 3.</p>
+                            <a href="#" class="btn btn-primary">Lihat Detail</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path fill="#ffff" fill-opacity="1" d="M0,128L21.8,117.3C43.6,107,87,85,131,106.7C174.5,128,218,192,262,218.7C305.5,245,349,235,393,218.7C436.4,203,480,181,524,165.3C567.3,149,611,139,655,154.7C698.2,171,742,213,785,213.3C829.1,213,873,171,916,144C960,117,1004,107,1047,122.7C1090.9,139,1135,181,1178,208C1221.8,235,1265,245,1309,229.3C1352.7,213,1396,171,1418,149.3L1440,128L1440,320L1418.2,320C1396.4,320,1353,320,1309,320C1265.5,320,1222,320,1178,320C1134.5,320,1091,320,1047,320C1003.6,320,960,320,916,320C872.7,320,829,320,785,320C741.8,320,698,320,655,320C610.9,320,567,320,524,320C480,320,436,320,393,320C349.1,320,305,320,262,320C218.2,320,175,320,131,320C87.3,320,44,320,22,320L0,320Z"></path>
+        </svg>
+    </section>
+    <!-- Akhir Project -->
+
+    <!-- Awal Sikils -->
+
+    <section id="skills">
+        <div class="container text-center mb-4">
+            <div class="row">
+                <h2>My Skils</h2>
+            </div>
+        </div>
+        <div class="container card pt-4 pb-4 bg-dark">
+        <div class="container text-center pb-2">
+        <img src="img/html.png" alt="profile" width="150" class=" img-thumbnail" />
+        <img src="img/html.png" alt="profile" width="150" class=" img-thumbnail" />
+        <img src="img/html.png" alt="profile" width="150" class=" img-thumbnail" />
+        <img src="img/html.png" alt="profile" width="150" class=" img-thumbnail" />
+        <img src="img/html.png" alt="profile" width="150" class=" img-thumbnail" />
+        <img src="img/html.png" alt="profile" width="150" class=" img-thumbnail" />
+        <img src="img/html.png" alt="profile" width="150" class=" img-thumbnail" />
+        <img src="img/html.png" alt="profile" width="150" class=" img-thumbnail" />
+        </div>
+        
+        </div>
+
+        </div>
+
+    </section>
+    <!-- Akhir Skills -->
+
+    <!-- Awal Contact -->
+
+
+    <!-- Akhir Contact -->
+
+    <footer class="bg-dark text-center text-white py-3">
+        <p>Copyright &copy; 2022
+            <a href="#" class="text-white">Nama Saya</a>. All
+    </footer>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
+    </script>
 </body>
+
 </html>
